674801a15cb533587a723f89c8e6841f4a11985c
Christian Hagedorn <christian.hagedorn@oracle.com>
2023-06-13 09:41:27 +0200

    add Test6.java

--- failures/run.sh
+++ failures/run.sh
@@ -9,7 +9,7 @@ NC='\033[0m'
 test_nr=1
 
 function check_crash() {
-    echo -n "Test$test_nr.java... "
+    echo -n "-> $1: "
     test_nr=$((test_nr+1))
     hs_err_file=$(find . -maxdepth 1 -name "hs_err_pid*")
     if [[ $hs_err_file ]]; then
@@ -22,22 +22,25 @@ function check_crash() {
 }
 
 
-$java -Xcomp -XX:CompileOnly=Test Test1.java > output$test_nr
-check_crash
+$java -Xcomp -XX:CompileCommand=compileonly,Test::* Test1.java > output$test_nr
+check_crash Test1.java
 
-$java -Xcomp -XX:CompileOnly=Test Test2.java > output$test_nr
-check_crash
+$java -Xcomp -XX:CompileCommand=compileonly,Test::* Test2.java > output$test_nr
+check_crash Test2.java
 
-$java -Xcomp -XX:CompileOnly=Test Test3.java > output$test_nr
-check_crash
+$java -Xcomp -XX:CompileCommand=compileonly,Test::* Test3.java > output$test_nr
+check_crash Test3.java
 
 # jdk-21+26-2230-tier6
-$java -Xcomp -XX:CompileOnly=Test Test4.java > output$test_nr
-check_crash
+$java -Xcomp -XX:CompileCommand=compileonly,Test::* Test4.java > output$test_nr
+check_crash Test4.java
 
 # jdk-21+24-2010-tier8 (currently attached as Test/Reduced.java to JDK-8308392)
-$java -Xcomp -XX:CompileOnly=Test Test5.java > output$test_nr
-check_crash
-$java -Xcomp -XX:CompileOnly=Reduced Reduced5.java > output$test_nr
-check_crash
+$java -Xcomp -XX:CompileCommand=compileonly,Test::* Test5.java > output$test_nr
+check_crash Test5.java
+$java -Xcomp -XX:CompileCommand=compileonly,Reduced::* Reduced5.java > output$test_nr
+check_crash Reduced5.java
+
+$java -Xcomp -XX:CompileCommand=compileonly,Test::* Test6.java > output$test_nr
+check_crash Test6.java
 
